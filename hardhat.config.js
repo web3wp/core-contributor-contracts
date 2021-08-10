e171a22ae26e84138fd310c526ce67544d9f8e53
Alex Connolly <alex@immutable.com>
2021-08-10 21:39:10 +1000

    registration wrapper

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,9 +8,13 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ROPSTEN_ALCHEMY_API_KEY}`,
-      accounts: [`0x${process.env.ROPSTEN_DEPLOYER_PRIVATE_KEY}`],
-    }
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+    },
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+    },
   }
 };
 
